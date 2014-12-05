e84e026a5a95fc0d4315552aea8059af7168ce83
Peter Qwarnstrom <peter.qwarnstrom@gmail.com>
2014-12-05 16:30:14 +0100

    Escape html in text

--- SimpleTweetMap.Web/TwitterConsumer.cs
+++ SimpleTweetMap.Web/TwitterConsumer.cs
@@ -68,7 +68,7 @@ namespace SimpleTweetMap.Web
 		{
 			if (tweet.Coordinates == null)
 				return;
-			var dto = new { coordinates = tweet.Coordinates, text = tweet.Text, sender = tweet.Creator.Name, time = tweet.CreatedAt };
+			var dto = new { coordinates = tweet.Coordinates, text = tweet.Text, sender = tweet.Creator.Name, time = tweet.CreatedAt, media = tweet.Media };
 
 			GlobalHost.ConnectionManager.GetHubContext<UserHub>().Clients.All.tweet(dto);
 		}
