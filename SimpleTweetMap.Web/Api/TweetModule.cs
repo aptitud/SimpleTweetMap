cea28f6b2d2de6d83db766c6fa4088d537722c5b
Peter Qwarnstrom <peter.qwarnstrom@gmail.com>
2014-12-05 14:22:44 +0100

    added appsettingsconfig

---
+++ SimpleTweetMap.Web/Api/TweetModule.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleTweetMap.Web.Api
+{
+	public class TweetModule : NancyModule
+	{
+		
+	}
+}
