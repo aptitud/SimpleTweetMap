5a3dda44fb73b8f55f117a19b10b8c30c9ccf74d
Peter Qwarnstrom <peter.qwarnstrom@gmail.com>
2014-12-05 14:50:35 +0100

    Fetch recent tweets

--- SimpleTweetMap.Web/TwitterConsumer.cs
+++ SimpleTweetMap.Web/TwitterConsumer.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
 using Tweetinvi;
+using Tweetinvi.Core.Enum;
 
 namespace SimpleTweetMap.Web
 {
@@ -30,18 +31,30 @@ namespace SimpleTweetMap.Web
 			// Setup your application credentials
 			TwitterCredentials.ApplicationCredentials = TwitterCredentials.CreateCredentials(accessToken, accessTokenSecret, consumerKey, consumerSecret);
 
+			//var tweets = Search.SearchTweets("Aptitud");
+
+			var searchParameter = Search.GenerateSearchTweetParameter("Aptitud_Sthlm");
+			//searchParameter.SearchType = SearchResultType.Recent;
+			searchParameter.MaximumNumberOfResults = 100;
+			var tweets = Search.SearchTweets(searchParameter);
+			
+			foreach (var tweet in tweets)
+			{
+				ParseTweet(tweet);
+			}
+
 			// Access the filtered stream
 			var filteredStream = Stream.CreateFilteredStream();
 			filteredStream.AddTrack("Aptitud_Sthlm");
-			filteredStream.MatchingTweetReceived += (sender, args) => { ParseTweet(args); };
+			filteredStream.MatchingTweetReceived += (sender, args) => { ParseTweet(args.Tweet); };
 			filteredStream.StartStreamMatchingAllConditions();
 		}
 
-		private static void ParseTweet(Tweetinvi.Core.Events.EventArguments.MatchedTweetReceivedEventArgs args)
+		private void ParseTweet(Tweetinvi.Core.Interfaces.ITweet tweet)
 		{
-			if (args.Tweet.Coordinates == null)
+			if (tweet.Coordinates == null)
 				return;
-			var dto = new { coordinates = args.Tweet.Coordinates, text = args.Tweet.Text, sender = args.Tweet.Creator.Name };
+			var dto = new { coordinates = tweet.Coordinates, text = tweet.Text, sender = tweet.Creator.Name };
 
 			GlobalHost.ConnectionManager.GetHubContext<UserHub>().Clients.All.tweet(dto);
 		}
